dde5bc71134be83667aab58238833aff2bd8ef7c
maazu <maaz581@gmail.com>
2022-09-21 18:39:03 +0500

    added calulator feature endpoints

--- backend/calculator/urls.py
+++ backend/calculator/urls.py
@@ -7,5 +7,11 @@ from calculator import views
 
 urlpatterns = [
     path('add/',  views.add, name='add'),
+    path('squareroot/',  views.square_root, name='squareroot'),
     path('subtract/',  views.subtract, name='subtract'),
+    path('multiply/',  views.multiply, name='multiply'),
+    path('divide/',  views.divide, name='divide'),
+    path('factorial/',  views.factorial, name='factorial'),
+    path('step4/',  views.step_four, name='step4'),
+    path('squarerootcap/',  views.square_root, name='squareroot'),
 ]
--- backend/calculator/views.py
+++ backend/calculator/views.py
@@ -4,10 +4,55 @@ from rest_framework.response import Response
 
 
 @api_view(['GET', 'POST'])
+def square_root(request):
+
+    if request.method == 'GET':
+        return Response("working subtract")
+
+    elif request.method == 'POST':
+        return Response("working subtract post")
+
+
+@api_view(['GET', 'POST'])
+def square_root_cap(request):
+
+    if request.method == 'GET':
+        return Response("working subtract")
+
+    elif request.method == 'POST':
+        return Response("working subtract post")
+
+
+@api_view(['GET', 'POST'])
+def module_limits_upload(request):
+
+    if request.method == 'GET':
+        return Response("working subtract")
+
+    elif request.method == 'POST':
+        return Response("working subtract post")
+
+
+@api_view(['GET', 'POST'])
+def step_four(request):
+    if request.method == 'GET':
+        return Response("working subtract")
+
+    elif request.method == 'POST':
+        return Response("working subtract post")
+
+
+'''
+
+Additional Calculator Feature Enpoints
+
+'''
+
+
+@api_view(['GET', 'POST'])
 def add(request):
 
     if request.method == 'GET':
-
         return Response("working add")
 
     elif request.method == 'POST':
@@ -18,7 +63,36 @@ def add(request):
 def subtract(request):
 
     if request.method == 'GET':
+        return Response("working subtract")
 
+    elif request.method == 'POST':
+        return Response("working subtract post")
+
+
+@api_view(['GET', 'POST'])
+def divide(request):
+
+    if request.method == 'GET':
+        return Response("working subtract")
+
+    elif request.method == 'POST':
+        return Response("working subtract post")
+
+
+@api_view(['GET', 'POST'])
+def multiply(request):
+
+    if request.method == 'GET':
+        return Response("working subtract")
+
+    elif request.method == 'POST':
+        return Response("working subtract post")
+
+
+@api_view(['GET', 'POST'])
+def factorial(request):
+
+    if request.method == 'GET':
         return Response("working subtract")
 
     elif request.method == 'POST':
