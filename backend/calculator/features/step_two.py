a9311ddb25e5d1b260f502e33001a8ca584b3dc5
maazu <maaz581@gmail.com>
2022-09-24 02:59:45 +0500

    mod limit function

--- backend/calculator/features/step_two.py
+++ backend/calculator/features/step_two.py
@@ -2,10 +2,18 @@ from .utils.file_upload import handle_fileupload
 from .basic_functions import compute_addition, compute_exponent, compute_mutliplication, compute_squareroot, compute_subtraction, compute_division, compute_factorial
 
 
+def compute_mod_limits(mod_limits_list):
+    return "Compute limits tomorrow"
+
+
 def compute_step_two(request):
     '''
-    Check Mod limit
+    Compute Mod limit 
+    by checking if the limit 
+    exist in the provided file
     '''
-    file = handle_fileupload(request)
-    print(file)
-    return ''
+    file_path = handle_fileupload(request)
+    file = open(file_path, "r")
+    file = file.read()
+    mod_limits_list = file.split(',')
+    return compute_mod_limits(mod_limits_list)
