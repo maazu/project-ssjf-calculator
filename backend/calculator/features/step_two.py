937ce49baf2baad8480985f4dc3b7be5cfc5c014
maazu <maaz581@gmail.com>
2022-09-25 14:34:17 +0500

    added additional endpoints

--- backend/calculator/features/step_two.py
+++ backend/calculator/features/step_two.py
@@ -1,10 +1,26 @@
+from decimal import Decimal
+from math import remainder
 from .utils.file_upload import handle_fileupload
-from .basic_functions import compute_addition, compute_exponent, compute_mutliplication, compute_squareroot, compute_subtraction, compute_division, compute_factorial
 
 
 def compute_mod_limits(test_number, mod_limits_list):
-    print(test_number, mod_limits_list)
-    return "Compute limits tomorrow"
+    mod_found = False
+    counter = 0
+    mod_result = ''
+    while not mod_found and counter < len(mod_limits_list):
+        mod_by = Decimal(mod_limits_list[counter])
+        print(test_number)
+        mod = float(mod_by % test_number)
+        mod_result = [number for number in mod_limits_list if Decimal(
+            number.strip()) == mod]
+
+        if len(mod_result) > 0:
+            mod_found = True
+            print("mode result", mod_result)
+            print(mod_result)
+            return mod_result[0]
+
+        counter = counter + 1
 
 
 def compute_step_two(request):
@@ -18,4 +34,5 @@ def compute_step_two(request):
     file = file.read()
     mod_limits_list = file.split(',')
     test_number = request.data['testNumber']
+    test_number = Decimal(test_number)
     return compute_mod_limits(test_number, mod_limits_list)
