146712be2e1aba7e2c93242f90cd7a9304f2a1bb
maazu <maaz581@gmail.com>
2022-09-28 18:13:46 +0500

    step 4 reinitailised

--- scripts/step4.py
+++ scripts/step4.py
@@ -1,4 +1,7 @@
 
+from utils.read_file import read_file_content, save_data_file, current_timestamp, str2bool
+from decimal import Decimal, getcontext
+import argparse
 import sys
 from decimal import *
 from utils.sorting import qsort
@@ -7,56 +10,48 @@ getcontext().prec = 999999999999999999
 getcontext().Emax = 999999999999999999
 
 
-def determine_distance_in_list(given_list):
-    distance = []
-    for i in range(len(given_list)-1):
-        # print(given_list[i], given_list[i+1])
-        d = abs(abs(given_list[i]) - abs(given_list[i+1]))
-        distance.append(d)
+getcontext().prec = 10000
+getcontext().Emax = 999999999999999999
 
-    distance = sum(list(set(distance)))
-
-    return distance
+CAL_OPERATION = 'step4'
+DEFAULT_RESULT_SAVE_PATH = '../results/' + CAL_OPERATION + \
+    '/' + current_timestamp() + '-' + CAL_OPERATION + '.txt'
 
 
-def compute_step_four_ints(number_one, mod_number):
-
-    first_step = []
-    first_mod_result = []
-    for i in range(1, mod_number+1):
-        first_mod = (i**2) % mod_number
-        # first_mod = first_mod * i
-        first_mod_result.append({i, first_mod})
-        first_step.append(first_mod)
-
-    # uniques values
-    first_step = qsort(list(set(first_step)))
-
-    pairs = []
-    for number in first_step:
-        for same_number in first_step:
-            subtract_value = same_number - number
-            if subtract_value == int(number_one) % mod_number:
-                pairs.append(number)
-                pairs.append(same_number)
-                # {0: [i == first_mod == 0]}
-
-    final_pairs = {key: [] for key in pairs}
-
-    for i in range(1, mod_number+1):
-        first_mod = (i**2) % mod_number
-        if first_mod in final_pairs:
-            final_pairs[first_mod].append(i)
-
-    return final_pairs
+def compute_step_four(testnumber, save_file, display_output, path=DEFAULT_RESULT_SAVE_PATH):
+    pass
 
 
 if __name__ == "__main__":
-    print('Running Step 4 Script')
+    parser = argparse.ArgumentParser(
+        description='Execute Step 3 of the program.')
 
-    # Test files
-    testnumber = '../data/testCaseNumbers/bill/' + "bill-char.txt"
-    # read_file(path)
-    number_one_file = testnumber
-    mod_number = 36
-    result = compute_step_four_ints(number_one_file, mod_number)
+    parser.add_argument('--testnumber', type=str,
+                        help='Enter a testnumber')
+
+    parser.add_argument("--modnumber", type=str2bool, nargs='?',
+                        const=True, default=False,
+                        help="print the result.")
+
+    parser.add_argument("--displayoutput", type=str2bool, nargs='?',
+                        const=True, default=False,
+                        help="print the result.")
+
+    parser.add_argument("--savefile", type=str2bool, nargs='?',
+                        const=True, default=False,
+                        help="print the result.")
+
+    args = parser.parse_args()
+
+    testnumber = args.testnumber
+    modnumber = args.modnumber
+    display_output = args.displayoutput
+    save_file = args.savefile
+
+    if testnumber.endswith('.txt'):
+        testnumber = read_file_content(testnumber)
+
+    # compute
+    print('Running Step 4 Script')
+    compute_step_four(testnumber, save_file=save_file,
+                      display_output=display_output)
