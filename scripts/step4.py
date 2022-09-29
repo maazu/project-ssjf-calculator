0dcb6ce7a873d38704aabd4a5f83a52bdec74c4c
maazu <maaz581@gmail.com>
2022-09-29 21:08:36 +0500

    updates

--- scripts/step4.py
+++ scripts/step4.py
@@ -4,7 +4,7 @@ from decimal import Decimal, getcontext
 import argparse
 import sys
 from decimal import *
-
+from numba import numba as nb
 sys.float_info.max
 getcontext().prec = 999999999999999999
 getcontext().Emax = 999999999999999999
@@ -15,8 +15,42 @@ DEFAULT_RESULT_SAVE_PATH = '../results/' + CAL_OPERATION + \
     '/' + current_timestamp() + '-' + CAL_OPERATION + '.txt'
 
 
-def compute_step_four(testnumber, save_file, display_output, path=DEFAULT_RESULT_SAVE_PATH):
-    pass
+def compute_step_four(testnumber, mod_number, save_file, display_output, path=DEFAULT_RESULT_SAVE_PATH):
+    mod_results = []
+    mod_number = int(mod_number)
+    testnumber = Decimal(testnumber)
+    for number in range(0, mod_number+1):
+        mod = number % mod_number
+        mod = mod * number
+        if mod >= mod_number:
+            mod_result = mod % mod_number
+            mod_results.append(mod_result)
+        else:
+            mod_result = mod
+            mod_results.append(mod_result)
+
+    unique_mods = list(set(mod_results))
+
+    found_pair = []
+    subtracted_mods = []
+    for number in unique_mods:
+        for same_number in unique_mods:
+
+            subtracted_number = same_number - number
+            if subtracted_number < 0:
+                subtracted_number = subtracted_number % mod_number
+
+            if subtracted_number == testnumber % mod_number:
+                found_pair.append(same_number)
+                found_pair.append(number)
+
+    if display_output:
+        print(found_pair)
+    
+    for number in range(0, mod_number+1):
+        s = found_pair
+
+    return found_pair
 
 
 if __name__ == "__main__":
@@ -26,9 +60,8 @@ if __name__ == "__main__":
     parser.add_argument('--testnumber', type=str,
                         help='Enter a testnumber')
 
-    parser.add_argument("--modnumber", type=str2bool, nargs='?',
-                        const=True, default=False,
-                        help="print the result.")
+    parser.add_argument("--modnumber",  type=str,
+                        help='Enter a mod number')
 
     parser.add_argument("--showoutput", type=str2bool, nargs='?',
                         const=True, default=False,
@@ -40,7 +73,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    testnumber = args.testnumber
+    testnumber = args.testnumber or ''
     modnumber = args.modnumber
     display_output = args.showoutput
     save_file = args.savefile
@@ -50,5 +83,5 @@ if __name__ == "__main__":
 
     # compute
     print('Running Step 4 Script')
-    compute_step_four(testnumber, save_file=save_file,
+    compute_step_four(testnumber, modnumber, save_file=save_file,
                       display_output=display_output)
