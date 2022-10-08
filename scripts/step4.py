433b92c829a24ba08c5dc67d5f44c338ee48a259
maazu <maaz581@gmail.com>
2022-10-08 15:12:19 +0500

    found smallest mod

--- scripts/step4.py
+++ scripts/step4.py
@@ -6,6 +6,12 @@ import argparse
 import sys
 from decimal import *
 import collections
+import numpy as np
+from numba import jit
+import random
+import timeit
+import numpy as np
+import pandas as pd
 sys.float_info.max
 getcontext().prec = 999999999999999999
 getcontext().Emax = 999999999999999999
@@ -18,6 +24,42 @@ DEFAULT_RESULT_SAVE_PATH = '../results/' + CAL_OPERATION + \
 
 def find_mod_equal_value(dict, query_value):
     return [key for key, value in dict.items() if value == query_value]
+
+
+@jit
+def merge(l1, l2):
+    combined = []
+    c1 = 0
+    c2 = 0
+    n1 = l1[c1]
+    n2 = l2[c2]
+    len1 = len(l1)
+    len2 = len(l2)
+    while (c1 < len1 and c2 < len2):
+        if n1 < n2:
+            combined.append(n1)
+            c1 += 1
+            if (c1 < len1):
+                n1 = l1[c1]
+        else:
+            combined.append(n2)
+            c2 += 1
+            if (c2 < len2):
+                n2 = l2[c2]
+    combined.extend(l1[c1:])
+    combined.extend(l2[c2:])
+    return combined
+
+
+@jit
+def mergesort(a):
+    n = len(a)
+    if n == 0:
+        return a
+    if n == 1:
+        return a
+    if n > 1:
+        return merge(mergesort(a[:int(n/2)]), mergesort(a[int(n/2):]))
 
 
 def compute_step_four(testnumber, mod_number, save_file, display_output, path=DEFAULT_RESULT_SAVE_PATH):
@@ -64,22 +106,50 @@ def compute_step_four(testnumber, mod_number, save_file, display_output, path=DE
             if subtracted_number == final_check_number:
                 print("This pair should be selected",
                       same_number, number, final_check_number)
-                found_pair.append(same_number)
-                found_pair.append(number)
+
+                found_pair.append(str(number) + "-" + str(same_number))
 
     print("Number where subtracted result found equal", found_pair)
 
     final_found_pairs = {}
 
-    for number in found_pair:
-        s = find_mod_equal_value(modding_dict, number)
-        final_found_pairs[number] = s
+    for pair in found_pair:
+        pair_split = pair.split('-')
+        p1 = pair_split[0]
+        p2 = pair_split[1]
+        pair_val_1 = find_mod_equal_value(modding_dict, int(p1))
+        pair_val_2 = find_mod_equal_value(modding_dict, int(p2))
+        print(p1, pair_val_1, p2, pair_val_2)
 
-    print("Found pairs")
-    final_found_pairs = collections.OrderedDict(
-        sorted(final_found_pairs.items()))
-    for i, k in final_found_pairs.items():
-        print(i, "\t\t", k)
+        final_smallest_mod = 'not found'
+
+        for number_one in pair_val_1:
+            all_results = np.array([], dtype='int64')
+            for number_two in pair_val_2:
+                subtract_out = number_two - number_one
+                print(number_two, number_one, subtract_out)
+                if subtract_out > 1:
+                    all_results = np.append(all_results, subtract_out)
+
+            if len(all_results) > 0:
+                found_smallest_mod = np.sort(all_results)
+                smallest_value = found_smallest_mod[0]
+                if final_smallest_mod == 'not found' and smallest_value > 0:
+                    final_smallest_mod = smallest_value
+                else:
+                    if final_smallest_mod > smallest_value:
+                        final_smallest_mod = smallest_value
+
+                print("Smallest Mod found in ", pair_val_2,
+                      found_smallest_mod[0])
+
+        print("Final smallest Mod found", final_smallest_mod)
+
+    print('Now we find the closest values in the gnerated pairs')
+
+
+# 6 - 1 = 5
+# 17 - 12 = 5
 
 
 if __name__ == "__main__":
